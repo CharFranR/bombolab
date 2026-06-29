beea14c74bf2cc8718e6487f3261fb6e61ae13e0
Oscar Francisco Reyes Guevara <charfranr@users.noreply.github.com>
2026-06-29 15:56:07 -0600

    chore(change name): hmatrix.rs -> isometry.rs

--- crates/bombolab-core/src/math/mod.rs
+++ crates/bombolab-core/src/math/mod.rs
@@ -1,9 +1,9 @@
 pub mod constants;
 pub mod dh;
 pub mod quaternion;
-pub mod hmatrix;
+pub mod isometry;
 
 pub use constants::{DEG_TO_RAD, EPS, FRAC_PI_2, FRAC_PI_4, PI, RAD_TO_DEG, TAU};
 pub use dh::{DHParameter, DHSolution, compute_a_matrix, solve};
 pub use quaternion::{Quaternion, solve_add, solve_subtract, solve_multiply, solve_divide};
-pub use hmatrix::{Movement, rotation_and_translation, translation_and_rotation, make_movement};
+pub use isometry::{Movement, rotation_and_translation, translation_and_rotation, make_movement};
